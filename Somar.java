3111c340c5d215cd65b4990e5d54e9c6df2e08ee
Davi de Araújo Dias <107774203+diasdavizin@users.noreply.github.com>
2023-03-20 20:21:41 -0300

    Add files via upload

---
+++ Somar.java
@@ -0,0 +1,22 @@
+package somar;
+import java.util.Scanner;
+public class Somar {
+    public static void main(String[] args) {
+        float n1,n2,soma;
+        Scanner teclado=new Scanner(System.in);
+        //entrada de dados
+        System.out.println("Digite o primeiro numero: ");
+        n1=teclado.nextFloat();
+        System.out.println("Digite o outro numero: ");
+        n2=teclado.nextFloat();
+        
+        //processamentos
+        soma=n1+n2;
+        
+        //saida
+        System.out.println("A soma dos numeros é: "+soma);
+    
+    
+    }
+    
+}
