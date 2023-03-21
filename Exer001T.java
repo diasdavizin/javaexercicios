50f6a73b202e17e87aa23714d480400a5c1991e9
Davi de Araújo Dias <107774203+diasdavizin@users.noreply.github.com>
2023-03-21 19:51:02 -0300

    Add files via upload

---
+++ Exer001T.java
@@ -0,0 +1,22 @@
+package exer001t;
+import java.util.Scanner;
+public class Exer001T {
+     public static void main(String[] args) {
+    
+    //variavéis 
+    int n1,n;
+    Scanner numero=new Scanner(System.in);
+    //entrada de dados
+    System.out.println("Digite um número: ");
+    n1=numero.nextInt();
+    
+    //processamento
+    n=n1*n1;
+    //saída de dados
+    System.out.println("O número digitado ao quadrado é: "+n);
+    
+    }
+    
+}
+	    
+
