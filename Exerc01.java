63fde1a592aa0e70b5b24ea651de438ffc70a927
Davi de Araújo Dias <107774203+diasdavizin@users.noreply.github.com>
2023-03-28 16:00:41 -0300

    Exerc01
    Faça um algoritmo/programa que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.

---
+++ Exerc01.java
@@ -0,0 +1,25 @@
+package exerc01;
+import java.util.Scanner;
+public class Exerc01 {
+    public static void main(String[] args) {
+        
+        //variaveis
+        float a,b,c;
+        Scanner teclado=new Scanner(System.in);
+        
+        //entrada de dados
+        System.out.println("Digite o valor de A: ");
+        a=teclado.nextFloat();
+        
+        System.out.println("Digite o valor de B: ");
+        b=teclado.nextFloat();
+        
+        System.out.println("Digite o valor de C: ");
+        c=teclado.nextFloat();
+        
+        //processamento
+        if (a+b<c){
+            System.out.println("A soma de A + B é menor.");
+        }
+    }
+}
